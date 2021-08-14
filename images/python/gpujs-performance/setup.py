e71e253acf6ac120f26b7a97db2af76805362baa
Roger Veciana <rveciana@gmail.com>
2021-08-14 23:27:00 +0200

    Updates

--- images/python/gpujs-performance/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-# Run python setup.py build_ext --inplace
-setup(name="interpolate_residuals",
-      ext_modules=cythonize('interpolate_residuals.pyx'),
-      requires=['numpy'])
