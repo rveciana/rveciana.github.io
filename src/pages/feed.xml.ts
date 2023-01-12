57a403234a172107b94ad5575c0b34029828e3b1
Roger Veciana Rovira <rveciana@gmail.com>
2023-01-12 23:58:05 +0100

    Add styles to rss amd clouds

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -3,15 +3,33 @@ import type { MarkdownInstance } from "astro";
 import config from "../config.json";
 import type { Frontmatter } from "../model";
 
-export const get = () => {
+export const get = async () => {
+  const postImportResult = import.meta.glob<MarkdownInstance<Frontmatter>>(
+    "../pages/*/*.m*",
+    {
+      eager: true,
+    }
+  );
+  const postsLists = Object.values(postImportResult);
+  const posts = postsLists
+    .sort(
+      (a, b) =>
+        new Date(b.frontmatter.pubDate).valueOf() -
+        new Date(a.frontmatter.pubDate).valueOf()
+    )
+    .slice(0, 10)
+    .map((post) => ({
+      link: post.url ?? "",
+      title: post.frontmatter.title,
+      description: post.frontmatter.description,
+      pubDate: new Date(post.frontmatter.pubDate),
+    }));
+
   return rss({
     title: config.title,
     description: config.description,
     site: config.url,
-    items: import.meta.glob<MarkdownInstance<Frontmatter>>("../pages/*/*.m*"),
+    items: posts,
+    stylesheet: "/styles.xsl",
   });
 };
-
-//   Object.values(allPosts).forEach(async (getInfo) => {
-//     const { url, frontmatter } = await getInfo();
-//     console.log(frontmatter);
